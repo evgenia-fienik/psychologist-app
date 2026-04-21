fb4bc0af5ba8e30b5c7f2eed72738c6c4a72002a
User <user@Users-MacBook-Pro.local>
2026-04-21 21:13:44 +0300

    add layout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
-import Layout from "./components/Layout/Layout.jsx";
+import Layout from "./components/layout/layout.jsx";
 import PsychologistsPage from "./pages/PsychologistsPage/PsychologistsPage.jsx";
 import FavoritesPage from "./pages/FavoritesPage/FavoritesPage.jsx";
 
